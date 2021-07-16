ae60e6fa867f0df6220db1aa64272cb8e35e5246
codewiz <c.chaiwat.ch@gmail.com>
2021-07-16 17:51:01 +0700

    add: footer

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn>Home</v-btn>
-      <v-btn>Login</v-btn>
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
     </v-app-bar>
     <!-- Login -->
     <v-content>
@@ -35,6 +35,32 @@
         </v-card-actions>
       </v-card>
   </v-content>
+  <v-footer
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="primary lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
   </v-app>
 </template>
 
@@ -47,7 +73,11 @@ export default {
   },
 
   data: () => ({
-    showPassword: false
+    showPassword: false,
+    links: [
+      'Home',
+      'Login'
+    ]
   }),
   methods: {
     toggleShowPassword () {
